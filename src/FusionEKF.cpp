d002031f6ceaa343e5d46d60618c6d84a8fd1d49
spillow <scott.pillow@gmail.com>
2017-07-01 00:04:36 -0700

    swap laser and radar.

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -139,14 +139,14 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
-    ekf_.H_ = H_laser_;
-    ekf_.R_ = R_laser_;
-    ekf_.Update(measures);
-  } else {
-    // Laser updates
     ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
     ekf_.R_ = R_radar_;
     ekf_.UpdateEKF(measures);
+  } else {
+    // Laser updates
+    ekf_.H_ = H_laser_;
+    ekf_.R_ = R_laser_;
+    ekf_.Update(measures);
   }
 
   // print the output
